3b6caa511b60e6d2e2339c604c5d7c92fb72c0af
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2020-10-25 21:44:55 -0300

    Implementar a função que acessa API de rotas

---
+++ src/api/rotas.js
@@ -0,0 +1,28 @@
+import {MAPBOX_ACCESS_TOKEN} from 'sisdle_mobile/src/api/constants'
+
+const routingProfiles = {
+    drivingTraffic : 'driving-traffic',
+    driving : 'driving',
+    walking : 'walking',
+    cycling : 'cycling'
+}
+
+async function getRota(profile, userPosition, destinationPosition) {
+    try {
+        let response = await fetch (
+            "https://api.mapbox.com/directions/v5/mapbox/" +
+            `${profile}/` +
+            `${userPosition.toString()};${destinationPosition.toString()}` +
+            "?geometries=geojson" +
+            `&access_token=${MAPBOX_ACCESS_TOKEN}`
+        )
+        let json = await response.json()
+        console.log(json)
+        return json.routes[0]
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export {getRota}
