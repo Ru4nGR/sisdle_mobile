ba7bfe9bc02b4afa750914dca78ba52b80be2057
Ruan Gomes do Rosario <ruangomesdorosario@gmail.com>
2021-02-23 18:25:51 -0300

    Diminui o tamanho da margem de podWrapper

--- src/components/LixeiraDrawer.tsx
+++ src/components/LixeiraDrawer.tsx
@@ -179,7 +179,7 @@ const styles = StyleSheet.create({
         backgroundColor : 'white'
     },
     podWrapper : {
-        marginHorizontal : 10,
+        marginHorizontal : 5,
         marginBottom : 10,
         backgroundColor : 'white',
         elevation : 7,
